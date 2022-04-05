259238c35caba473146246cb0d6080494bc284f6
LireiBro <slava-nesterov-1990@bk.ru>
2022-04-05 18:06:47 +0700

    first commit

---
+++ Assets/SimpleSkeleton/script/SimpleSkeletonController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSkeletonController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private int speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    Animator animator;
+    SkinnedMeshRenderer skinnedMesh;
+
+    private int lineToMove = 1;
+    public float lineDistance = 4;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (SwipeController.swipeRight)
+        {
+            if (lineToMove < 2)
+                lineToMove++;
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+
+        if (SwipeController.swipeUp)
+        {
+            if (controller.isGrounded)
+                Jump();
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if (lineToMove == 2)
+            targetPosition += Vector3.right * lineDistance;
+
+        transform.position = targetPosition;
+    }
+
+    private void Jump()
+    {
+        dir.y = jumpForce;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        dir.y += gravity * Time.fixedDeltaTime;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
+
